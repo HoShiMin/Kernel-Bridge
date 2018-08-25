1018418d5c89659378bb996a5c25336d03983398
HoShiMin <aleksanderem@yandex.ru>
2018-08-25 20:51:53 +0300

    Added SMEP support in KbExecuteShellCode

--- Kernel-Bridge/API/KernelShells.cpp
+++ Kernel-Bridge/API/KernelShells.cpp
@@ -4,6 +4,7 @@
 
 #include "GetProcAddress.h"
 #include "MemoryUtils.h"
+#include "CPU.h"
 
 namespace KernelShells {
     _IRQL_requires_max_(APC_LEVEL)
@@ -11,16 +12,29 @@ namespace KernelShells {
         _ShellCode Shell,
         OPTIONAL IN OUT PVOID Argument
     ) {
+        static volatile LONG EnteringsCount = 0;
+        InterlockedIncrement(&EnteringsCount);
+
         ULONG Result = 0;
 
         KFLOATING_SAVE FpuState = {};
         BOOLEAN FpuSaved = KeSaveFloatingPointState(&FpuState) == STATUS_SUCCESS;
+
+        KAFFINITY PreviousAffinity = KeQueryActiveProcessors();
+        KeSetSystemAffinityThread(1); // Executing on 1st core of 1st processor
+
+        if (CPU::IsSmepPresent()) CPU::DisableSmep();
 
         __try {
             Result = Shell(Importer::GetKernelProcAddress, Argument);
         } __except (EXCEPTION_EXECUTE_HANDLER) {
             Result = static_cast<ULONG>(-1);
         }
+
+        InterlockedDecrement(&EnteringsCount);
+        if (!InterlockedCompareExchange(&EnteringsCount, 0, 0) && CPU::IsSmepPresent()) CPU::EnableSmep();
+
+        KeSetSystemAffinityThread(PreviousAffinity);
 
         if (FpuSaved) KeRestoreFloatingPointState(&FpuState);
 
